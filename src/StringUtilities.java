a46921f76145a792e0068b1f688a5f49bc808e22
LanCorC <lanc3.corcega@gmail.com>
2023-10-10 20:07:44 +1300

    Section 24.407(Debugging and Unit Testing: Introduction to Debugging): code demonstration, walked through some features of IntelliJ's debugger functionality

--- src/StringUtilities.java
+++ src/StringUtilities.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class StringUtilities {
+public class StringUtilities {
+    private StringBuilder sBuilder = new StringBuilder();
+    private int charsAdded = 0;
+
+    public void addChar(StringBuilder sBuilder, char c) {
+        this.sBuilder.append(c);
+        charsAdded++;
+    }
 }
